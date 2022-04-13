cca951929b65a6ca6b6cacb11d05539e81ed3b17
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2022-04-13 17:31:41 +0200

    Add notNull, hasValue, and equal filters

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,71 @@
-function add(a: number, b: number): number {
-  return a + b;
+import getValueByKey from "get-value-key";
+
+/**
+ *
+ * @param item
+ * @returns
+ */
+function notNull<T>(item: T): boolean {
+  return item != null;
+}
+/**
+ *
+ * @param key
+ * @returns
+ */
+notNull.on = function <T>(key: string) {
+  return function (item: T) {
+    return notNull(getValueByKey(item, key));
+  };
+};
+
+/**
+ *
+ * @param item
+ * @returns
+ */
+function hasValue<T>(item: T): boolean {
+  return Boolean(item) || (typeof item === "number" && item === 0);
+}
+/**
+ *
+ * @param key
+ * @returns
+ */
+hasValue.on = function <T>(key: string) {
+  return function (item: T) {
+    return hasValue(getValueByKey(item, key));
+  };
+};
+
+/**
+ *
+ * @param value
+ * @returns
+ */
+function equal<T>(value: T) {
+  /**
+   *
+   * @param item
+   * @returns
+   */
+  function fn(item: T | null): boolean {
+    // TODO: add deep equality for non-primitive types
+    return item === value;
+  }
+
+  /**
+   *
+   * @param key
+   * @returns
+   */
+  fn.on = function <T>(key: string) {
+    return function (item: T) {
+      return fn(getValueByKey(item, key));
+    };
+  };
+
+  return fn;
 }
 
-export default add;
+export { notNull, hasValue, equal };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,135 @@
 import { describe, it, expect } from "vitest";
 
-import add from "../src";
+import { notNull, hasValue, equal } from "../src";
 
-describe("add", () => {
+describe("notNull", () => {
   it("is a function", () => {
-    expect(typeof add).toBe("function");
+    expect(typeof notNull).toBe("function");
   });
 
-  it("adds 2 number", () => {
-    const expected = 4;
-    const actual = add(2, 2);
+  it("filters items that are falsy (except 0)", () => {
+    const arr = ["hello", 0, null, "", 22];
+
+    const expected = ["hello", 0, "", 22];
+    const actual = arr.filter(notNull);
 
     expect(actual).toEqual(expected);
+  });
+
+  describe("notNull.on", () => {
+    it("is a function", () => {
+      expect(typeof notNull.on).toBe("function");
+    });
+
+    it("returns a function", () => {
+      expect(typeof notNull.on("")).toBe("function");
+    });
+
+    it("filters items that are falsy (except 0)", () => {
+      const arr = [
+        { name: "Bob", age: 23 },
+        { name: "Alice", age: 32 },
+        null,
+        { name: "Tom", age: 0 },
+        { name: "Candice" },
+      ];
+
+      const expected = [
+        { name: "Bob", age: 23 },
+        { name: "Alice", age: 32 },
+        { name: "Tom", age: 0 },
+      ];
+      const actual = arr.filter(notNull.on("age"));
+
+      expect(actual).toEqual(expected);
+    });
+  });
+});
+
+describe("hasValue", () => {
+  it("is a function", () => {
+    expect(typeof hasValue).toBe("function");
+  });
+
+  it("filters items that are falsy (except 0)", () => {
+    const arr = ["hello", 0, null, "", 22];
+
+    const expected = ["hello", 0, 22];
+    const actual = arr.filter(hasValue);
+
+    expect(actual).toEqual(expected);
+  });
+
+  describe("hasValue.on", () => {
+    it("is a function", () => {
+      expect(typeof hasValue.on).toBe("function");
+    });
+
+    it("returns a function", () => {
+      expect(typeof hasValue.on("")).toBe("function");
+    });
+
+    it("filters items that are falsy (except 0)", () => {
+      const arr = [
+        { name: "Bob", age: 23 },
+        { name: "Alice", age: 32 },
+        null,
+        { name: "Tom", age: 0 },
+        { name: "Candice" },
+      ];
+
+      const expected = [
+        { name: "Bob", age: 23 },
+        { name: "Alice", age: 32 },
+        { name: "Tom", age: 0 },
+      ];
+      const actual = arr.filter(hasValue.on("age"));
+
+      expect(actual).toEqual(expected);
+    });
+  });
+});
+
+describe("equal", () => {
+  it("is a function", () => {
+    expect(typeof equal).toBe("function");
+  });
+
+  it("returns a function", () => {
+    expect(typeof equal("")).toBe("function");
+  });
+
+  it("filters for values that are equal to the provided value", () => {
+    const arr = ["hello", 0, null, "", 22, 0];
+
+    const expected = [0, 0];
+    const actual = arr.filter(equal<number | string | null>(0));
+
+    expect(actual).toEqual(expected);
+  });
+
+  describe("equal().on", () => {
+    it("is a function", () => {
+      expect(typeof equal("").on).toBe("function");
+    });
+
+    it("returns a function", () => {
+      expect(typeof equal("").on("")).toBe("function");
+    });
+
+    it("filters for values that are equal to the provided value", () => {
+      const arr = [
+        { name: "Bob", age: 23 },
+        { name: "Alice", age: 32 },
+        null,
+        { name: "Tom", age: 60 },
+        { name: "Candice" },
+      ];
+
+      const expected = [{ name: "Bob", age: 23 }];
+      const actual = arr.filter(equal("Bob").on("name"));
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
